cf23e6076c4a5aabb3cc702292b0fb6dcec15b12
YuktiJain123 <69969529+YuktiJain123@users.noreply.github.com>
2020-10-18 16:02:36 +0530

    Add files via upload

--- rope.js
+++ rope.js
@@ -10,7 +10,7 @@ class Rope{
         }
        
         //console.log(options);
-        this.rope=constraint.create(options);
+        this.rope=Constraint.create(options);
         World.add(world,this.rope);
     }
     display(){
--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-
+const Constraint = Matter.Constraint
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
